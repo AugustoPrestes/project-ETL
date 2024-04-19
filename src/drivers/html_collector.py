359beb7261b90895c7cc6870bdd19ffcac1367fb
Augusto Prestes <57774809+AugustoPrestes@users.noreply.github.com>
2024-04-19 02:39:47 +0000

    feat: Add HtmlCollector

---
+++ src/drivers/html_collector.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+from zipfile import ZipFile
+from typing import List, Dict
+
+class HtmlCollector():
+    def collect_essential_information(self, html: str) -> List[Dict[str, str]]:
+        # Instanciando o bs4
+        soup = BeautifulSoup(html, 'html.parser')
+
+        # Armazenando os arquivos .zip
+        file_ziped = soup.find_all('a')
+
+        # Iterando sobre os arquivos .zip
+        dfp_list = []
+        for file in file_ziped:
+            
+            file_name = file.contents[0] # Pegando o nome do arquivo
+            link = file.get('href')      # Pegando o link do arquivo 
+            
+            if '.zip' in link:      # Salvando apenas os arquivos .zip
+                dfp_list.append({
+                    "file": file_name,  # Salvando o nome do arquivo 
+                    "link": link        # Salvando o complemento do arquivo 
+                })
+ 
+
+        return dfp_list
--- src/drivers/http_requester.py
+++ src/drivers/http_requester.py
@@ -4,8 +4,9 @@ from typing import Dict
 class HttpRequester:
     def __init__(self) -> None:
         self.__url =  'https://dados.cvm.gov.br/dados/CIA_ABERTA/DOC/DFP/DADOS/'
+        # self.__url =  'https://web.archive.org/web/20121007172955/http://www.nga.gov/collection/anZ1.htm' # URL para acessar o site utilizado no projeto original
 
-    def request_from_page(self) -> Dict: 
+    def request_from_page(self) -> Dict[int, str]: 
         resposnse = requests.get(self.__url)
         
         return {
