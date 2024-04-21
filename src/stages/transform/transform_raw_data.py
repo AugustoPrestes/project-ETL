149f87a2af7fd49528a519855d504ecb79389ce6
Augusto Prestes <57774809+AugustoPrestes@users.noreply.github.com>
2024-04-21 23:00:28 +0000

    feat: Salvando os arquivos .parquet corretamente

--- src/stages/transform/transform_raw_data.py
+++ src/stages/transform/transform_raw_data.py
@@ -17,7 +17,6 @@ class TransformRawData:
 
     def __transform_raw_data(self, extract_contract: ExtractContract) -> List[Dict]:
         extraction_content = extract_contract.transent_information_content
-        extraction_length = extract_contract.transent_information_length
         extraction_date = extract_contract.extraction_date
         
         transformed_data = []
@@ -32,13 +31,14 @@ class TransformRawData:
 
             name = full_name[0]         # Pegando apenas o nome sem a extenção (.zip)
             
+            os.makedirs(f'/workspaces/project-ETL/src/data/bronze/{name}', exist_ok=True)
             print(f"\nNome do arquivo: {name}\nCaminho para o download: {link}")
 
             transformed_data = self.__download_raw_data(name, link)
+            transformed_data['extraction_date'] = extraction_date
 
             print()
             print(transformed_data)
-            break
 
     def __download_raw_data(self, name: str, link: str)-> Dict: # Retorna o ano, o nome e a quantidade de arquivos
         
@@ -48,44 +48,43 @@ class TransformRawData:
         with tempfile.TemporaryDirectory(dir='/workspaces/project-ETL/src/data', prefix='temp_',suffix=f'_{name}', ) as tempdir:
             # Baixando o ziped
             wget.download(link, out=tempdir)
-            print("Baixei")
+            # print("Baixei")
             
             # Abrindo zipe file dentro do temp_dir
             with ZipFile(f"{tempdir}/{name}.zip", 'r') as zip_file:
-                print("Abri o zip")
+                # print("Abri o zip")
                 # Extraindo todos os arquivos para a temp_dir    
                 zip_file.extractall(path=tempdir)
-                print("Extrai tudo do zip")
+                # print("Extrai tudo do zip")
 
                 # Tratando de cada arquivo
                 for f in os.listdir(tempdir):
-                    print("Loop dos arquivos temporarios (.csv)")
+                    # print("Loop dos arquivos temporarios (.csv)")
                     full_name = f.split('.')
                     f = full_name[0]
-                    file = self.__transform_layer_bronze(file=f, path=tempdir)
-                    list_tables.append(file)
+                    self.__transform_layer_bronze(file=f, path=tempdir, name=name) # Salvando os arquivos em .parquet na camada bronze
             
+                # Retornando os meta dados de cada arquivos
                 return {
                     "dir_name": name,
                     "dir_path": f'/workspaces/project-ETL/src/data/bronze/' + name,
                     "list_files": os.listdir(tempdir),
-                    "content_tables": list_tables,
                     "link_origin": link,
                     "transform_date": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
                 }
                 
-    def __transform_layer_bronze(self, file, path: str):
-        print("função layer_bronze")
+    def __transform_layer_bronze(self, file, path: str, name: str) -> None:
+        """
+        file: Nome do arquivo que será transacionado,
+        path: Caminho no qual o arquivo se encontra atualmente (temp_dir),
+        name: Nome da pasta do ano, utilizada para salvar o .parquet
+        """
+        # print("função layer_bronze")
         df = pd.read_csv(f"{path}/{file}.csv", sep=';', encoding='latin-1')
-        print("Carreguei o arquivo .csv")
+        # print("Carreguei o arquivo .csv")
 
-        os.makedirs('/workspaces/project-ETL/src/data/bronze', exist_ok=True)
-        print("Criei a pasta da camada bronze ")
+        # print("Criei a pasta da camada bronze ")
 
-        d1 = df.to_dict('list')
-
-        df.to_parquet(f'/workspaces/project-ETL/src/data/bronze/{file}.parquet')
-        print("Criei o arquivo .parquet\n")
-
-        return dict(d1)
+        df.to_parquet(f'/workspaces/project-ETL/src/data/bronze/{name}/{file}.parquet')
+        
                 
